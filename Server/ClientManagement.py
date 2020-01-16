1ef542418883db6e4079f9d3460b65d4759d17b2
JuYeolYang <56315335+JuYeolYang@users.noreply.github.com>
2020-01-16 13:11:31 +0900

    Update ClientManagement.py

--- Server/ClientManagement.py
+++ Server/ClientManagement.py
@@ -23,7 +23,7 @@ class ClientManagement(Thread):
                 ClientManagement.user_list.remove(self.__client_connectionSock)
 
                 # sendData is message for another client
-                sendData = "{} is disconnected.".format(self.__client_connectionSock)
+                sendData = "{} is disconnected.".format(self.__client_connectionSock.getpeername())
 
                 # Check in server
                 print("[Server] " + sendData)
@@ -35,3 +35,25 @@ class ClientManagement(Thread):
                 print(self.__client_connectionSock.getpeername(), " :", recvData)
                 for client_object in ClientManagement.user_list:
                     client_object.send(recvData.encode('utf-8'))
+
+
+port = 60000
+
+
+print("test")
+serverSock = socket(AF_INET, SOCK_STREAM)
+serverSock.bind(('', port))
+serverSock.listen(1)
+
+print('%d번 포트로 접속 대기중...' % (port))
+
+connectionSock, addr = serverSock.accept()
+print(str(addr), '에서 접속되었습니다.')
+
+
+cm = ClientManagement(connectionSock)
+cm.start()
+print("ClientManagement 객체인 cm이 정상적으로 작동함")
+
+cm.join()
+print("Program Close")
