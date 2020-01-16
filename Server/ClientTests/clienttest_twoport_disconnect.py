635fdc518fda851d8d6d45e96ae4fc2264cf116d
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2020-01-16 13:30:42 +0900

    Add files via upload

---
+++ Server/ClientTests/clienttest_twoport_disconnect.py
@@ -0,0 +1,24 @@
+from socket import *
+
+port1 = 60000
+port2 = 65000
+
+clientSock1 = socket(AF_INET, SOCK_STREAM)
+clientSock1.connect(('aihost.kr', port1))
+
+recvData = clientSock1.recv(1024).decode('utf-8')
+print("[Client] Received " + str(recvData))
+
+clientSock1.send("300".encode("utf-8"))
+print("[Client] Sent " + str(300))
+
+clientSock2 = socket(AF_INET, SOCK_STREAM)
+clientSock2.connect(('aihost.kr', port2))
+
+print('접속 완료')
+
+input("Enter to disconnect")
+
+clientSock2.send("!!exit()".encode('utf-8'))
+
+
