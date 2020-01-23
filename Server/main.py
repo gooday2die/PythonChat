d73499199befdada7f3158d40e72d2458abe3921
JuYeolYang <56315335+JuYeolYang@users.noreply.github.com>
2020-01-23 13:30:41 +0900

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -15,19 +15,60 @@ import time
 
 """
 global message_user_list
+global message_user_name_dict
 message_user_list = list()
+message_user_name_dict = dict()
 
 class ClientManagement(Thread):
     # user_list : Store every client, include myself
     # user_name : Asign name each client
     user_list = message_user_list
-    user_name = dict()
+    user_name = message_user_name_dict
 
     def __init__(self, client_connectionSock):
         Thread.__init__(self)
         self.__client_connectionSock = client_connectionSock
 
     def run(self):
+        while True:
+            try:
+                # Create client nickname
+                self.__client_connectionSock.send("[Server] Make your nickname: ".encode('utf-8'))
+
+                self.__client_connectionSock.settimeout(5)
+                user_nickname = self.__client_connectionSock.recv(1024).decode('utf-8')
+
+                # Check client nickname
+                self.__client_connectionSock.send("[Server] Is {} your nickname? : 1. yes | 2. no")
+                clientAnswer = self.__client_connectionSock.recv(1024).decode('utf-8')
+
+                # If client send '1' or 'yes', message_user_name_dict is added a client nickname
+                if clientAnswer == '1' or clientAnswer.lower() == 'yes':
+                    user_name[self.__client_connectionSock] = user_nickname
+                    self.__client_connectionSock.send("[Server] Connected!!".encode('utf-8'))
+                    self.__client_connectionSock.send("[Server] Your nickname is successfuly made")
+
+                    message_user_list.append(message_connection_sock)
+
+                    print("[Server] IP " + str(handshake_connection_sock.getpeername()) +
+                        " was added to user list")
+                    break
+
+                # If client send '2' or 'no', send a messgae to client
+                elif clientAnswer == '2' or clientAnswer.lower() == 'no':
+                    self.__client_connectionSock.send("[Server] Nickname didn't match".encode('utf-8'))
+                    self.__client_connectionSock.send("[Server] Try again".encode('utf-8'))
+                    continue
+
+                # If client send a wrong message, send a message to client that you send a wrong message
+                else:
+                    self.__client_connectionSock.send("[Server] Sorry, your code has been entered incorrectly. Try again")
+                    continue
+
+            except Exception as e:
+                print("Error code : {}".format(e))
+                self.__client_connectionSock.send("[Server] Server Error")
+
         while True:
             # temp_recvData is decoded message
             # recvData is encoded message
@@ -37,7 +78,7 @@ class ClientManagement(Thread):
                 if recvData == '!!exit()':
                     # Remove clientsock
                     ClientManagement.user_list.remove(self.__client_connectionSock)
-
+                    del user_nickname[self.__client_connectionSock]
                     # sendData is message for another client
                     sendData = "{} is disconnected.".format(self.__client_connectionSock)
 
@@ -48,13 +89,14 @@ class ClientManagement(Thread):
                         client_object.send(sendData.encode('utf-8'))
                     break
                 else:
-                    print(self.__client_connectionSock.getpeername(), " :", recvData)
+                    print(user_nickname[self.__client_connectionSock], " :", recvData)
                     for client_object in ClientManagement.user_list:
-                        client_object.send(recvData.encode('utf-8'))
+                        client_object.send((user_nickname[self.__client_connectionSock] + " : " + recvData).encode('utf-8'))
             
             except ConnectionError:
                 ClientManagement.user_list.remove(self.__client_connectionSock)
-                print("Remove exit client")
+                del user_nickname[self.__client_connectionSock]
+                print("[Server] Remove exit client")
                 break
 
             except:
@@ -62,23 +104,24 @@ class ClientManagement(Thread):
 
 class StartNewConnections:
     def __init__(self, handshake_port, message_port):
-        self.handshake_port = handshake_port
-        self.message_port = message_port
+        # handshake_port : check connection port, message_port : send a message port
+        self.__handshake_port = handshake_port
+        self.__message_port = message_port
 
-        self.handshake_sock = socket(AF_INET, SOCK_STREAM)
-        self.message_sock = socket(AF_INET, SOCK_STREAM)
+        self.__handshake_sock = socket(AF_INET, SOCK_STREAM)
+        self.__message_sock = socket(AF_INET, SOCK_STREAM)
 
-        self.handshake_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.message_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.__handshake_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.__message_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
-        print("[Server] Handshake port : " + str(self.handshake_port))
-        print("[Server] Message port : " + str(self.message_port))
+        print("[Server] Handshake port : " + str(self.__handshake_port))
+        print("[Server] Message port : " + str(self.__message_port))
         print("[Server] Starting Server...")
 
 
         try:
-            self.handshake_sock.bind(('', self.handshake_port))
-            self.message_sock.bind(('', self.message_port))
+            self.__handshake_sock.bind(('', self.__handshake_port))
+            self.__message_sock.bind(('', self.__message_port))
             print("[Server] Successfully binded sock")
 
         except:
@@ -86,31 +129,38 @@ class StartNewConnections:
             exit()
 
     def check_new_connections(self):
-        self.handshake_sock.listen(1)
-        self.handshake_sock.settimeout(3)
+        self.__handshake_sock.listen(1)
+        self.__handshake_sock.settimeout(3)
 
         try:
-            handshake_connection_sock, handshake_addr = self.handshake_sock.accept()
+            handshake_connection_sock, handshake_addr = self.__handshake_sock.accept()
             print("[Server] IP " + str(handshake_addr) + " connected.")
 
+            """
+            A method for getting new connections
+            When a new connection is set, server sends 200
+            If server gets 300 in return, it saves the socket object
+            into the user list for future use.
+            @param none
+            @return none
+            """
             handshake_connection_sock.send('200'.encode('utf-8'))
             handshake_connection_sock.settimeout(5)
             temp_data = handshake_connection_sock.recv(1024).decode('utf-8')
 
-            if temp_data == '300':
-                self.message_sock.settimeout(5)
-                self.message_sock.listen(1)
+            try:
+                if temp_data == '300':
+                    self.__message_sock.settimeout(5)
+                    self.__message_sock.listen(1)
 
-                message_connection_sock, message_addr = self.message_sock.accept()
-                message_user_list.append(message_connection_sock)
+                    message_connection_sock, message_addr = self.__message_sock.accept()
+                    self.__make_new_connection_thread(message_connection_sock)
 
-                print("[Server] IP " + str(handshake_connection_sock.getpeername()) +
-                      " was added to user list")
-
-                self.__make_new_connection_thread(message_connection_sock)
-                print("[Server] Successful make thread!")
-            else:
-                print("[Server] Wrong code returned")
+                    print("[Server] Successful make thread!")
+                else:
+                    print("[Server] Wrong code returned")
+            except:
+                pass
 
         except Exception as ex:
             # print("Exception Error", ex)
@@ -126,5 +176,8 @@ if __name__ == "__main__":
     SNC = StartNewConnections(60000, 65000)
     while True:
         SNC.check_new_connections()
+        print(message_user_list)
+        '''
         for client in message_user_list:
             print(client.getpeername())
+        '''
