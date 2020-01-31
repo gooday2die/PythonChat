fdf912aba64c079765f793aaebf17671ed77300b
JuYeolYang <56315335+JuYeolYang@users.noreply.github.com>
2020-01-31 15:20:30 +0900

    Update TestClient.py

--- Server/ClientTests/TestClient.py
+++ Server/ClientTests/TestClient.py
@@ -1,7 +1,24 @@
 from socket import *
+from threading import Thread
 
 port1 = 60000
 port2 = 65000
+
+
+class Test(Thread):
+    def __init__(self, message_sock):
+        Thread.__init__(self)
+        self.message_sock = message_sock
+
+    def run(self):
+        while True:
+            try:
+                message = self.message_sock.recv(1024).decode('utf-8')
+                print(message)
+
+            except Exception as e:
+                print("Error code: ", e)
+                break
 
 clientSock1 = socket(AF_INET, SOCK_STREAM)
 clientSock1.connect(('localhost', port1))
@@ -17,6 +34,8 @@ clientSock2.connect(('localhost', port2))
 
 print('접속 완료')
 
+Test(clientSock2).start()
+
 while True:
     try:
         send_massage = input("[Client] Send: ")
@@ -25,8 +44,5 @@ while True:
             break
         else:
             clientSock2.send(send_massage.encode('utf-8'))
-
-            received_massage = clientSock2.recv(1024).decode('utf-8')
-            print("[Client] Received :", received_massage)
     except:
         break
